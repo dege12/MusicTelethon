987862f52c6563beb052d1ba80e1f9af36aa9cb1
yddde <93260463+yddde@users.noreply.github.com>
2021-12-24 20:21:11 +0200

    Add files via upload

---
+++ MusicTelethon/الريبو.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, SUDO_USERS
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (    ("Minggu", 60 * 60 * 24 * 7),    ("Hari", 60 * 60 * 24),    ("Jam", 60 * 60),    ("Menit", 60),    ("Detik", 1),)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+
+@Client.on_message(    filters.user(SUDO_USERS) & filters.command(["restart","zaid","تحديث"], prefixes=f"{HNDLR}"))
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("**𝐎𝐧𝐞**")
+    await loli.edit("**𝐓𝐰𝐨**")
+    await loli.edit("**𝐓𝐡𝐫𝐞𝐞**")
+    await loli.edit("**𝐅𝐨𝐮𝐫**")
+    await loli.edit("**𝐅𝐢𝐯𝐞**")
+    await loli.edit("**𝐒𝐢𝐱**")
+    await loli.edit("**𝐒𝐞𝐯𝐞𝐧**")
+    await loli.edit("**𝐄𝐢𝐠𝐡𝐭**")
+    await loli.edit("**𝐍𝐢𝐧𝐞**")
+    await loli.edit("**𝐌𝐚𝐝𝐚 𝐮𝐬𝐞𝐫𝐛𝐨𝐭 𝐢𝐬 𝐒𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲 𝐫𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝🧚‍♀**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+@Client.on_message(filters.command(["اوامر","help","مدى"], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+    await m.delete()
+    HELP = f"""
+<b>**- مرحباً {m.from_user.mention}**
+
+ ⤃𝐌𝐚𝐝𝐚 𝐌𝐮𝐬𝐢𝐜 𝐔𝐬𝐞𝐫𝐛𝐨𝐭🧚‍♂⤂
+𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅✭𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅
+
+𓏬 | لتشغيل صوتية في المكالمة أرسل ↞ ⊰ `{HNDLR}تشغيل  + اسم الاغنية` ⊱
+𓏬 | لتشغيل فيديو في المكالمة  ↞ ⊰ `{HNDLR}تشغيل_فيديو  + اسم الاغنية` ⊱
+𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅✭𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅
+
+𓏬 | لأيقاف الاغنية او الفيديو مؤقتآ  ↞ ⊰ `{HNDLR}مؤقت` ⊱ 
+𓏬 | لأعاده تشغيل الاغنية ↞  ⊰ `{HNDLR}متابعه` ⊱
+𓏬 | لأيقاف الاغنية  ↞ ⊰ `{HNDLR}ايقاف` ⊱ 
+𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅✭𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅
+
+𓏬 | لتحميل صوتية أرسل ↞ ⊰ `{HNDLR}تحميل + اسم الاغنية او الرابط` ⊱
+𓏬 | لتحميل فيديو  ↞  ⊰ `{HNDLR}فيديو + اسم الاغنية او الرابط` ⊱
+𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅✭𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅
+
+𓏬 | لتخطي الاغنيه ↞ ⊰ `{HNDLR}التالي`  ⊱
+𓏬 | لعرض معلومات السورس ↞  ⊰ `{HNDLR}سورس` ⊱
+𓏬 | لتشغيل 10 اغاني عشوائيه ↞ ⊰ `{HNDLR}عشوائيه` + معرف القناه او القروب  ⊱
+𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅✭𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅𓐅
+- ➮ [𝐁𝐨𝐭 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⚜](t.me/MQQQS)  """
+    await m.reply(HELP, disable_web_page_preview=True)
+@Client.on_message(filters.command(["سورس","المطور","repo","alive"], prefixes=f"{HNDLR}"))
+async def repo(client, m: Message):
+    await m.delete()
+    REPO = f"""
+<b>⇸ **𝐖𝐞𝐥𝐜𝐨𝐦𝐞 ❪ {m.from_user.mention} ❫**
+
+**༄ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 ↠** [𝐙 𝐀 𝐈 𝐃 🎼](tg://user?id=1518220300)\n**༄ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ↠** [𝐌𝐚𝐝𝐚  𝐔𝐬𝐞𝐫𝐛𝐨𝐭 🧚‍♂](t.me/MQQQS) .
+
+"""
+    await m.reply(REPO, disable_web_page_preview=True)
+@Client.on_message(filters.command(["المطورين"], prefixes=f"{HNDLR}"))
+async def repo(client, m: Message):
+    await m.delete()
+    DEVS = f"""
+<b>⇸ **𝐖𝐞𝐥𝐜𝐨𝐦𝐞 ❪ {m.from_user.mention} ❫**
+
+**- المطور الأساسي : [𝐙 𝐀 𝐈 𝐃 🎼](tg://user?id=1518220300)\n༄ [𝐆𝐘𝐃𝐀𝐀 🧚‍♂‍](t
+me/IGIDD)**\n༄ [𝐄 𝐒 𝐒 𝐀 𝄬](t.me/E_4_B) .
+
+"""
+    await m.reply(DEVS, disable_web_page_preview=True)
+        
